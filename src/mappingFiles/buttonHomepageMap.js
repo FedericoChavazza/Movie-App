5469a5a83c947c634264b1606290b853f2752394
Federico <81062792+FedericoChavazza@users.noreply.github.com>
2022-04-05 16:57:37 -0300

    final changes to movie details

--- src/mappingFiles/buttonHomepageMap.js
+++
@@ -1,5 +0,0 @@
-export const buttonHomepageMap = [
-  { title: 'homepage.navBar.trending', value: 'trending' },
-  { title: 'homepage.navBar.discover', value: 'discover' },
-  { title: 'homepage.navBar.top_rated', value: 'top_rated' },
-];

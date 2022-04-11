cc3b03d33113ed5c361e849c330645ff938957ff
Federico <81062792+FedericoChavazza@users.noreply.github.com>
2022-04-11 15:21:05 -0300

    now movie-details with endpoints and styles

--- src/pages/MovieInfo.js
+++ src/pages/MovieInfo.js
@@ -12,6 +12,7 @@ import Modal from 'components/modalComponent/Modal';
 import { useState } from 'react';
 import { Slider } from 'components/viewComponents/Slider';
 import { useMovieDetailQuery, useImageMovieDetailQuery } from 'services/api';
+import { BiArrowBack } from 'react-icons/bi';
 
 export function MovieInfo() {
   const [openModalState, setOpenModalState] = useState(false);
@@ -43,83 +44,85 @@ export function MovieInfo() {
 
   return (
     <div className="MovieInfo-container">
-      <Button customClass="MovieInfo__btn" handleClick={() => goBack()}>
-        {t('movieDetails.btn.back')}
-      </Button>
-      <div className="MovieInfo__data">
-        <div className="MovieInfo-data__container">
-          <div>
-            {' '}
-            <img
-              className="MovieInfo__img"
-              src={`${process.env.REACT_APP_ORIGINAL_IMG}${movieData?.backdrop_path}`}
-              alt={movie.img}
-            />{' '}
-          </div>
-          <div className="MovieInfo-data___information">
-            <div className="MovieInfo__title">
+      <div className="MovieInfo-data__holeContainer">
+        <Button customClass="MovieInfo__btn" handleClick={() => goBack()}>
+          <BiArrowBack size={25} />
+        </Button>
+        <div className="MovieInfo__data">
+          <div className="MovieInfo-data__container">
+            <div>
               {' '}
-              <h2>{movieData?.original_title} </h2>{' '}
-              <Button img={<BsFillBookmarkFill />}> {t('movieDetails.btn.add')} </Button>
-            </div>
-            <p> {movieData?.overview} </p>
-            <div className="MovieInfo__genres">
-              {' '}
-              {movieData?.genres.map((genre, i) => {
-                return <div key={i}> {genre.name} </div>;
-              })}{' '}
-            </div>
-            <div className="MovieInfo__rate">
-              {' '}
-              <div className="MovieInfo__imdbRating">
-                {' '}
-                <h4 className="MovieInfo-rating__title">{t('movieDetails.ratings.imdb')}</h4>{' '}
-                <h4 className="MovieInfo__ratingStar">
-                  {' '}
-                  <AiFillStar size={25} /> {`${movie.imdbRating} /10`}{' '}
-                </h4>
-                <h5>{movie.allRates} </h5>
-              </div>
-              <div className="MovieInfo-display__rating">
-                <h4 className="MovieInfo-rating__title">{t('movieDetails.ratings.user')}</h4>
-                <h4 className="MovieInfo__ownRating">
-                  {' '}
-                  <AiOutlineStar size={25} /> Rate{' '}
-                </h4>
-              </div>
-              <div className="MovieInfo-display__rating">
-                <h4 className="MovieInfo-rating__title">{t('movieDetails.ratings.general')}</h4>
-                <div className="MovieInfo__stadisticRate">
-                  {' '}
-                  <h4> {movieData?.vote_average}</h4>
-                </div>{' '}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="Movie-carousel__container">
-        <h2> {t('movieDetails.images')} </h2>
-        <div ref={scrollRef} className="MovieInfo__extraImgs">
-          {' '}
-          {imageData?.backdrops.map((img, i) => {
-            return (
               <img
-                aria-hidden="true"
-                onClick={() => imageHandler(img.file_path)}
-                src={`${process.env.REACT_APP_ORIGINAL_IMG}${img.file_path}`}
-                alt={i}
-                key={i}
-              />
-            );
-          })}{' '}
+                className="MovieInfo__img"
+                src={`${process.env.REACT_APP_ORIGINAL_IMG}${movieData?.backdrop_path}`}
+                alt={movie.img}
+              />{' '}
+            </div>
+            <div className="MovieInfo-data___information">
+              <div className="MovieInfo__title">
+                {' '}
+                <h2>{movieData?.original_title} </h2>{' '}
+                <Button img={<BsFillBookmarkFill />}> {t('movieDetails.btn.add')} </Button>
+              </div>
+              <p> {movieData?.overview} </p>
+              <div className="MovieInfo__genres">
+                {' '}
+                {movieData?.genres.map((genre, i) => {
+                  return <div key={i}> {genre.name} </div>;
+                })}{' '}
+              </div>
+              <div className="MovieInfo__rate">
+                {' '}
+                <div className="MovieInfo__imdbRating">
+                  {' '}
+                  <h4 className="MovieInfo-rating__title">{t('movieDetails.ratings.imdb')}</h4>{' '}
+                  <h4 className="MovieInfo__ratingStar">
+                    {' '}
+                    <AiFillStar size={25} /> {`${movie.imdbRating} /10`}{' '}
+                  </h4>
+                  <h5>{movie.allRates} </h5>
+                </div>
+                <div className="MovieInfo-display__rating">
+                  <h4 className="MovieInfo-rating__title">{t('movieDetails.ratings.user')}</h4>
+                  <h4 className="MovieInfo__ownRating">
+                    {' '}
+                    <AiOutlineStar size={25} /> Rate{' '}
+                  </h4>
+                </div>
+                <div className="MovieInfo-display__rating">
+                  <h4 className="MovieInfo-rating__title">{t('movieDetails.ratings.general')}</h4>
+                  <div className="MovieInfo__stadisticRate">
+                    {' '}
+                    <h4> {movieData?.vote_average}</h4>
+                  </div>{' '}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        <div className="Movie-carousel__container">
+          <h2> {t('movieDetails.images')} </h2>
+          <div ref={scrollRef} className="MovieInfo__extraImgs">
+            {' '}
+            {imageData?.backdrops.map((img, i) => {
+              return (
+                <img
+                  aria-hidden="true"
+                  onClick={() => imageHandler(img.file_path)}
+                  src={`${process.env.REACT_APP_ORIGINAL_IMG}${img.file_path}`}
+                  alt={i}
+                  key={i}
+                />
+              );
+            })}{' '}
+          </div>
+        </div>
+        {openModalState && (
+          <Modal show={openModalState} onClose={() => setOpenModalState(false)}>
+            <Slider imgIndex={imageInArray} imgArray={imageData?.backdrops} img={selectedImage} />
+          </Modal>
+        )}
       </div>
-      {openModalState && (
-        <Modal show={openModalState} onClose={() => setOpenModalState(false)}>
-          <Slider imgIndex={imageInArray} imgArray={imageData?.backdrops} img={selectedImage} />
-        </Modal>
-      )}
     </div>
   );
 }

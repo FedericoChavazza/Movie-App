f303aa8974a06fc25b8b9839c32757f69ab32091
Federico <81062792+FedericoChavazza@users.noreply.github.com>
2022-04-19 17:55:16 -0300

    Fixing img alt styles (#11)
    * aa
    
    * changes done when image is loading, now is animated

--- src/components/viewComponents/Movie.js
+++ src/components/viewComponents/Movie.js
@@ -5,14 +5,18 @@ export function Movie({ title, date, img, icon, id }) {
   const watchlist = getWatchlist();
   return (
     <div className="Movie-container">
+      {img ? (
+        <img
+          className="Movie-container__img"
+          src={`${process.env.REACT_APP_ORIGINAL_IMG}${img}`}
+          alt="movie"
+        />
+      ) : (
+        <div className="Movie-container--img-notfound"> </div>
+      )}
       {watchlist.map(value => {
         return value.id === id && <div className="Movie__bookmark"> {icon} </div>;
       })}
-      <img
-        className="Movie-container__img"
-        src={`${process.env.REACT_APP_ORIGINAL_IMG}${img}`}
-        alt="movie"
-      />
       <div className="Movie-container__details">
         <p className="Movie-container__title"> {title} </p>
         <div className="Movie-container__date"> {date} </div>

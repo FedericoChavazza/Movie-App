ac21b200aa919c72762bcf239d0ce7ac0c0db14c
Federico <81062792+FedericoChavazza@users.noreply.github.com>
2022-04-20 15:15:59 -0300

    Update src/components/routes/RouteFromPath.js
    Co-authored-by: Julian Pasquale <julianagustin78@gmail.com>

--- src/components/routes/RouteFromPath.js
+++ src/components/routes/RouteFromPath.js
@@ -1,10 +1,7 @@
 import { Route } from 'react-router-dom';
 import { node } from 'prop-types';
 
-
-const RouteFromPath = ({ component, ...route }) => (
-  <Route {...route}> {component} </Route>
-);
+const RouteFromPath = ({ component, ...route }) => <Route {...route}>{component}</Route>;
 
 RouteFromPath.propTypes = {
   component: node.isRequired,

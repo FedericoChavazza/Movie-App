f8300303b6fc9ccad718ed7fab038278727dac65
Federico Chavazza <federicochavazza@MacBook-Pro-de-Federico.local>
2022-04-25 17:46:39 -0300

    now image opacity working when images takes its time loading

--- src/components/viewComponents/Movie.js
+++ src/components/viewComponents/Movie.js
@@ -1,21 +1,31 @@
 import './styles.scss';
 import { getWatchlist } from 'utils/api';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/opacity.css';
+
+import { useState } from 'react';
 
 export function Movie({ title, date, img, icon, id, language }) {
   const watchlist = getWatchlist();
+  const [loaded, setLoaded] = useState(false);
+
+  console.log(loaded, 'soy los loading');
+
   return (
     <div className="Movie-container">
       {watchlist.map(value => {
         return value.id === id && <div className="Movie__bookmark"> {icon} </div>;
       })}
-      {img ? (
-        <img
-          className="Movie-container__img"
+      {!loaded ? (
+        <LazyLoadImage
+          alt=""
+          effect="opacity"
           src={`${process.env.REACT_APP_ORIGINAL_IMG}${img}`}
-          alt="movie"
         />
       ) : (
-        <div className="Movie-container--img-notfound"> </div>
+        <div className="Movie-container--img-loading" onLoad={() => setLoaded(true)}>
+          {' '}
+        </div>
       )}
       <div className="Movie-container__details">
         <p className="Movie-container__title"> {title} </p>

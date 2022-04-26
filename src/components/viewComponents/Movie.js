f292e467338a1d14cf9f609ebb54f3a6f6ee9dce
Federico Chavazza <federicochavazza@MacBook-Pro-de-Federico.local>
2022-04-26 16:12:27 -0300

    deleted extra code

--- src/components/viewComponents/Movie.js
+++ src/components/viewComponents/Movie.js
@@ -3,27 +3,17 @@ import { getWatchlist } from 'utils/api';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/opacity.css';
 
-import { useState } from 'react';
-
 export function Movie({ title, date, img, icon, id, language }) {
   const watchlist = getWatchlist();
-  const [loaded, setLoaded] = useState(false);
+
   return (
     <div className="Movie-container">
       {watchlist.map(value => {
         return value.id === id && <div className="Movie__bookmark"> {icon} </div>;
       })}
-      {!loaded ? (
-        <LazyLoadImage
-          alt=""
-          effect="opacity"
-          src={`${process.env.REACT_APP_ORIGINAL_IMG}${img}`}
-        />
-      ) : (
-        <div className="Movie-container--img-loading" onLoad={() => setLoaded(true)}>
-          {' '}
-        </div>
-      )}
+
+      <LazyLoadImage alt="" effect="opacity" src={`${process.env.REACT_APP_ORIGINAL_IMG}${img}`} />
+
       <div className="Movie-container__details">
         <p className="Movie-container__title"> {title} </p>
         <div className="Movie-container__date"> {date} </div>
